bb48de5ddc833c97bc4023afb0ad3b06352da763
ZioPhil <37614860+ZioPhil@users.noreply.github.com>
2023-02-23 12:32:12 +0100

    Add files via upload

--- authorizationServer/src/main.rs
+++ authorizationServer/src/main.rs
@@ -6,9 +6,107 @@ use std::str::from_utf8;
 use identity_iota::account::{Account, AccountBuilder};
 use identity_iota::iota_core::IotaDID;
 use bstr::B;
+use tokio::runtime::Runtime;
 use crate::lib::create_challenge;
 
 mod lib;
+
+fn handle_client(mut stream: TcpStream, issuer: Account) {
+    let rt  = Runtime::new().unwrap();
+    let mut data = [0 as u8; 1047]; //1047 byte buffer
+    'foo: while match stream.read(&mut data) {
+        Ok(size) => {
+            let msg = from_utf8(&data[0..size]).unwrap();
+            println!("\nReceived instruction: {}", msg);
+
+            match msg {
+                "vc" => {
+                    stream.write(b"ack").unwrap();
+                    match stream.read(&mut data) {
+                        Ok(size) => {
+                            let did = from_utf8(&data[0..size]).unwrap();
+
+                            let user_did: IotaDID = match IotaDID::parse(did) {
+                                Ok(did) => did,
+                                Err(err) => {
+                                    eprintln!("Error: {:?}", err);
+                                    return
+                                },
+                            };
+
+                            rt.block_on(async {
+                                let vc: String = match lib::crea_vc(&issuer, &user_did).await {
+                                    Ok(vc) => vc,
+                                    Err(err) => {
+                                        eprintln!("Error: {:?}", err);
+                                        return
+                                    }
+                                };
+                                println!("VC created!");
+                                stream.write(B(vc.as_str())).unwrap();
+                            });
+                        }
+                        Err(err) => {
+                            eprintln!("Error: {:?}", err);
+                            return
+                        }
+                    }
+                },
+                "vp" => {
+                    let challenge = lib::create_challenge();
+                    println!("Challenge created!");
+                    stream.write(B(challenge.0.as_str())).unwrap();
+
+                    match stream.read(&mut data) {
+                        Ok(..) => println!(),
+                        Err(err) => {
+                            eprintln!("Error: {:?}", err);
+                            return
+                        },
+                    };
+
+                    stream.write(B(challenge.1.to_rfc3339().as_str())).unwrap();
+
+                    let vp: &str = match stream.read(&mut data) {
+                        Ok(size) => from_utf8(&data[0..size]).unwrap(),
+                        Err(err) => {
+                            eprintln!("Error: {:?}", err);
+                            return
+                        },
+                    };
+                    println!("Received VP from client.");
+
+                    rt.block_on(async {
+                        match lib::verify_vp(&String::from(vp), &issuer, challenge).await {
+                            Ok(..) => {
+                                println!("VP verified!");
+                                //A questo punto il client si è autenticato
+                            },
+                            Err(err) => {
+                                eprintln!("Vp not validated: {:?}", err);
+                                return
+                            },
+                        };
+                    });
+                },
+                "shutdown" => {
+                    println!("Terminating connection with {}", stream.peer_addr().unwrap());
+                    break 'foo
+                },
+                _ => {
+                    println!("\nUnknown input received");
+                },
+            }
+            true
+        },
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+    stream.shutdown(Shutdown::Both).unwrap();
+}
 
 #[tokio::main]
 async fn main() {
@@ -66,16 +164,6 @@ async fn main() {
             return
         },
     };
-    let issuer: Account = match lib::load_identity(&mut builder, issuer_did).await {
-        Ok(identity) => {
-            println!("Identity loaded! DID: {}", identity.did());
-            identity
-        },
-        Err(err) => {
-            eprintln!("Error: {:?}", err);
-            return
-        },
-    };
 
     let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
     println!("\nServer listening on port 3333");
@@ -84,95 +172,21 @@ async fn main() {
             Ok(mut stream) => {
                 println!("New connection: {}", stream.peer_addr().unwrap());
 
-                let mut data = [0 as u8; 1047]; //1047 byte buffer
-                'foo: while match stream.read(&mut data) {
-                    Ok(size) => {
-                        let msg = from_utf8(&data[0..size]).unwrap();
-                        println!("\nReceived instruction: {}", msg);
-
-                        match msg {
-                            "vc" => {
-                                stream.write(b"ack").unwrap();
-                                match stream.read(&mut data) {
-                                    Ok(size) => {
-                                        let did = from_utf8(&data[0..size]).unwrap();
-
-                                        let user_did: IotaDID = match IotaDID::parse(did) {
-                                            Ok(did) => did,
-                                            Err(err) => {
-                                                eprintln!("Error: {:?}", err);
-                                                return
-                                            },
-                                        };
-
-                                        let vc: String = match lib::crea_vc(&issuer, &user_did).await {
-                                            Ok(vc) => vc,
-                                            Err(err) => {
-                                                eprintln!("Error: {:?}", err);
-                                                return
-                                            }
-                                        };
-                                        println!("VC created!");
-                                        stream.write(B(vc.as_str())).unwrap();
-                                    }
-                                    Err(err) => {
-                                        eprintln!("Error: {:?}", err);
-                                        return
-                                    }
-                                }
-                            },
-                            "vp" => {
-                                let challenge = lib::create_challenge();
-                                println!("Challenge created!");
-                                stream.write(B(challenge.0.as_str())).unwrap();
-
-                                match stream.read(&mut data) {
-                                    Ok(..) => println!(),
-                                    Err(err) => {
-                                        eprintln!("Error: {:?}", err);
-                                        return
-                                    },
-                                };
-
-                                stream.write(B(challenge.1.to_rfc3339().as_str())).unwrap();
-
-                                let vp: &str = match stream.read(&mut data) {
-                                    Ok(size) => from_utf8(&data[0..size]).unwrap(),
-                                    Err(err) => {
-                                        eprintln!("Error: {:?}", err);
-                                        return
-                                    },
-                                };
-                                println!("Received VP from client.");
-
-                                match lib::verify_vp(&String::from(vp), &issuer, challenge).await {
-                                    Ok(..) => {
-                                        println!("VP verified!");
-                                        //A questo punto il client si è autenticato
-                                    },
-                                    Err(err) => {
-                                        eprintln!("Vp not validated: {:?}", err);
-                                        return
-                                    },
-                                };
-                            },
-                            "shutdown" => {
-                                println!("Terminating connection with {}", stream.peer_addr().unwrap());
-                                break 'foo
-                            },
-                            _ => {
-                                println!("\nUnknown input received");
-                            },
-                        }
-                        true
+                let issuer: Account = match lib::load_identity(&mut builder, issuer_did.clone()).await {
+                    Ok(identity) => {
+                        println!("Identity loaded! DID: {}", identity.did());
+                        identity
                     },
-                    Err(_) => {
-                        println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
-                        stream.shutdown(Shutdown::Both).unwrap();
-                        false
-                    }
-                } {}
-                stream.shutdown(Shutdown::Both).unwrap();
+                    Err(err) => {
+                        eprintln!("Error: {:?}", err);
+                        return
+                    },
+                };
+
+                thread::spawn(move|| {
+                    //connection succeeded
+                    handle_client(stream, issuer)
+                });
             }
             Err(e) => {
                 println!("Error: {}", e);
